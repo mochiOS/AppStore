7b01509a2d1aed8c96cd3d8cef01587ce2709832
tas0dev <156896971+tas0dev@users.noreply.github.com>
2026-06-06 16:39:29 +0900

    oauthのコンフィグテンプレ追加

---
+++ config/oauth.example.php
@@ -0,0 +1,20 @@
+<?php
+
+// Please replace this secrets and please rename this file to `oauth.php`
+
+return [
+    'github' => [
+        'client_id' => 'xxxxxxxx',
+        'client_secret' => 'xxxxxxxx',
+    ],
+
+    'gitlab' => [
+        'client_id' => 'xxxxxxxx',
+        'client_secret' => 'xxxxxxxx',
+    ],
+
+    'google' => [
+        'client_id' => 'xxxxxxxx',
+        'client_secret' => 'xxxxxxxx',
+    ],
+];
